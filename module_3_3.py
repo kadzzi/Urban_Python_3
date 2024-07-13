52fc9f9a074571f73b4afc6043b0f170a387a437
Dmitry <kadzzi@gmail.com>
2024-07-13 12:47:04 +0300

    module_3_3 PEP8

--- module_3_3.py
+++ module_3_3.py
@@ -1,12 +1,13 @@
-def print_params(a = 1, b = 'строка', c = True):
+def print_params(a=1, b='строка', c=True):
     print(a, b, c)
+
 
 print_params('cat', [1, 5])
 print_params(*[34, "dog"], 23.15)
 print_params()
 
-print_params(b = 25)
-print_params(c = [1,2,3])
+print_params(b=25)
+print_params(c=[1, 2, 3])
 
 values_list = ['dodo', 5, 34.1]
 values_dict = {'a': 'dront', 'b': 6, 'c': 35.2}
